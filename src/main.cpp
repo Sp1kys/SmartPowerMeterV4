a9b8fc7ae289f6574f79d437ace9d4dbab3faad9
Sp1kys <robert.seredenko@gmail.com>
2022-10-29 15:26:50 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -201,7 +201,7 @@ void readandwrite()
     apparentEnergy = apparentEnergy + energyVals.ApparentEnergyValue/1000;
 
     //String serverName1 = "http://192.168.50.154:8080/input/post?node=esp32v4&fulljson=";
-    String serverName1 = "http://192.168.32.107:8080/api/socket"; ///api/socket
+    String serverName1 = "http://192.168.32.107:8080/api/socket";
     //String serverName1 = "http://ptsv2.com/t/yx1ay-1666997073/post";
     String Json = "";
 
@@ -238,7 +238,7 @@ void readandwrite()
     http.begin(wifiClient, serverName1);
     Serial.println(serverName1);
     http.addHeader("Content-Type", "application/json");
-    int httpResponseCode = http.POST(Json);//"node=1&data="+ Json +"&apikey=1acc2f443e9c0c49a8211d33e14ac873"
+    int httpResponseCode = http.POST(Json);
 
     //int httpResponseCode = http.GET();
     Serial.print("HTTP Response code: ");
